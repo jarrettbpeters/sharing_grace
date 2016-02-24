c61c569a0c36e720eaaddfb121bae1a7de473caf
andrew-breunig <andrew.breunig@gmail.com>
2016-02-24 11:59:00 -0500

    removed redundant controller variables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-give_post_categories = GivePost.create([{category: "Car Seats, Strollers, & Trailers"}, {category: "Diapering"}, {category: "Monitors & Nursery"}, {category: "Feeding"}, {category: "Carriers & Wraps"}, {category: "Sleeping"}])
